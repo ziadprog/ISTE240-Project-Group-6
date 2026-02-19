9528f023464d8f2c8b0dbdd6ef4dc3d6a29f23a4
Ziad Abdelrahman <ziadamirxc@gmail.com>
2026-02-19 20:10:33 +0400

    Add files via upload

---
+++ src/main/java/com/example/assignment/model/Article.java
@@ -0,0 +1,37 @@
+package com.example.assignment.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class Article {
+    private String title;
+    private String author;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+}
---
+++ src/main/java/com/example/assignment/model/resources.java
@@ -0,0 +1,71 @@
+package com.example.assignment.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+public class resources {
+    private String name;
+    private String type; // HOTLINE, FOUNDATION, APPLICATION, SUPPORT_GROUP, WEBSITE
+    private String description;
+    private String contactInfo;
+    private String website;
+    private String phoneNumber;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
