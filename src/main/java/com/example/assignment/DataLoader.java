24e07a45a9d5ab0c9d7a4051dddc0698d74d1821
ShahidBora <sb4851@rit.edu>
2026-04-26 18:50:14 +0400

    merge

--- src/main/java/com/example/assignment/DataLoader.java
+++ src/main/java/com/example/assignment/DataLoader.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DataLoader {
-//
+
 
     private final UserRepository userRepository;
     private final DiaryRepository diaryRepository;
